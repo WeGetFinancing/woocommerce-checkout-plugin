b3e333de1bccac0dba8623ac50da4a91e7a129a7
Riccardo De Leo <r@rdeleo.com>
2024-03-27 17:09:53 +0100

    GET-2556 | refactor(woocommerce block support): updated plugin version

--- wegetfinancing-checkout.php
+++ wegetfinancing-checkout.php
@@ -11,9 +11,9 @@
  * Plugin Name:       WeGetFinancing Payment Gateway
  * Plugin URI:        https://github.com/WeGetFinancing/woocommerce-checkout-plugin
  * Description:       Integrate WeGetFinancing payment gateway to woocommerce
- * Version:           1.3.1
+ * Version:           1.4.0
  * Requires at least: 5.0
- * Requires PHP:      8.0
+ * Requires PHP:      8.1
  * Author:            Riccardo De Leo
  * Author URI:        https://github.com/WeGetFinancing
  * Text Domain:       wegetfinancing-payment-gateway
